335d8780ed2acb4ddbea1b7ceff9b25d0af7565b
Emerson Niño <emerson.nino@pragma.com.co>
2022-03-29 22:04:09 -0500

    Ojala sirva

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Main() {
     return (
         <>
             <div>
-                Main
+                <Link to="/login">Login</Link>
             </div>
         </>
     )
